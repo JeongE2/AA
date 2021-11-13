71e33188746cb5948bc2bbbf8e29ad36c006ce63
Seon <sunju701@gmail.com>
2021-11-13 16:51:37 +0900

    first

---
+++ app/src/main/java/com/example/aa/OutActivity.java
@@ -0,0 +1,22 @@
+package com.example.aa;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class OutActivity extends Fragment {
+    public OutActivity(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.out, container, false);
+    }
+}
