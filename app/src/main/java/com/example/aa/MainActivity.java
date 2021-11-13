b38aedb560d0a68ffd21cc706a3e85b6316d2f98
Seon <sunju701@gmail.com>
2021-11-13 16:19:48 +0900

    first

--- app/src/main/java/com/example/aa/MainActivity.java
+++ app/src/main/java/com/example/aa/MainActivity.java
@@ -1,16 +1,82 @@
 package com.example.aa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button study, out, daily, adver, mypage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this, "안녕", Toast.LENGTH_SHORT).show();
+        study = findViewById(R.id.study);
+        out = findViewById(R.id.out);
+        daily = findViewById(R.id.daily);
+        adver = findViewById(R.id.adver);
+        mypage = findViewById(R.id.mypage);
+
+        study.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                StudyActivity study = new StudyActivity();
+                transaction.replace(R.id.frame, study);
+                transaction.commit();
+            }
+        });
+
+        out.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+               OutActivity out = new OutActivity();
+                transaction.replace(R.id.frame, out);
+                transaction.commit();
+            }
+        });
+
+        daily.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                DailyActivity daily = new DailyActivity();
+                transaction.replace(R.id.frame, daily);
+                transaction.commit();
+            }
+        });
+
+        adver.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                AdverActivity adver = new AdverActivity();
+                transaction.replace(R.id.frame, adver);
+                transaction.commit();
+            }
+        });
+
+        mypage.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                MypageActivity mypage = new MypageActivity();
+                transaction.replace(R.id.frame, mypage);
+                transaction.commit();
+            }
+        });
+
+
+
+
     }
 }
