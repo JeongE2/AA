a17d42ca738f018d07e6bcd9c5dab26fd7b148ab
Seon <sunju701@gmail.com>
2021-11-15 00:12:48 +0900

    login

--- app/src/main/java/com/example/aa/AdverActivity.java
+++ app/src/main/java/com/example/aa/AdverActivity.java
@@ -1,23 +1,44 @@
 package com.example.aa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class AdverActivity extends Fragment {
+public class AdverActivity extends Fragment implements View.OnClickListener {
 
-    public AdverActivity(){
+    /*public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }*/
+
+    public AdverActivity() {
 
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.adver, container, false);
+
+        View v = inflater.inflate(R.layout.adver, container, false);
+        Button btn_write = v.findViewById(R.id.btn_writing);
+        btn_write.setOnClickListener(this);
+        return v;
     }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btn_writing:
+                Intent intent = new Intent(getActivity(), WritingActivity.class);
+                startActivity(intent);
+                break;
+        }
+
+        };
 }
